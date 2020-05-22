8eb6776f46822fcd333b44ab9d48a9674971ed63
Matthew-Ielusic <matthew38@optonline.net>
2020-05-22 16:46:24 -0700

    Make GenPoly actually, like, work

---
+++ GenPolyTests.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+from GeneralizedPoly import GenPoly
+
+print("Testing add...")
+
+
+def testAdd(x, y, expected):
+    actual = x + y
+    if not (actual.coefficients == expected.coefficients):
+        print("add failed")
+        print("x: ", x, sep='')
+        print("y: ", y, sep='')
+        print("sum:      ", actual, sep='')
+        print("expected: ", expected, sep='')
+
+x =   GenPoly([1,0])
+y =   GenPoly([1,0,1])
+sum = GenPoly([0,0,1])
+testAdd(x, y, sum)
+
+x =   GenPoly([1,1,1,0,1,0,1,0])
+y =   GenPoly([1,1,0,0,0,0,0,1])
+sum = GenPoly([0,0,1,0,1,0,1,1])
+testAdd(x,y,sum) 
+
+x =   GenPoly([1, 0, 1, 0, 0, 1, 0, 1])
+y =   GenPoly([0, 1, 0, 1, 1, 1])
+sum = GenPoly([1, 1, 1, 1, 1, 0, 0, 1])
+testAdd(x, y, sum)
+
+print("Done.")
+
+print("Testing multiplication...")
+
+def testMult(x, y, expected):
+    actual = x * y
+    if not (actual.coefficients == expected.coefficients):
+        print("multiplication failed")
+        print("x: ", x, sep='')
+        print("y: ", y, sep='')
+        print("product:  ", actual, sep='')
+        print("expected: ", expected, sep='')
+
+x =   GenPoly([1,1,1,0,1,0,1,0])
+y =   GenPoly([1,1,0,0,0,0,0,1])
+prd = GenPoly([1,0,0,1,1,1,1,0,1,1,0,1,0,1])
+testMult(x,y,prd) 
+
+x   = GenPoly([1, 0, 0, 0])
+y   = GenPoly([0, 1, 1, 1, 1, 1, 1])
+prd = GenPoly([0, 1, 1, 1, 1, 1, 1])
+testMult(x,y,prd) 
+
+x   = GenPoly([1, 0, 0, 0])
+y   = GenPoly([0, 1, 1, 1, 1, 1, 1])
+prd = GenPoly([0, 1, 1, 1, 1, 1, 1])
+testMult(x,y,prd) 
+
+x   = GenPoly([0, 1, 0, 0])
+y   = GenPoly([0, 1, 1, 1, 1, 1, 1])
+prd = GenPoly([0, 0, 1, 1, 1, 1, 1, 1])
+testMult(x,y,prd) 
+
+x   = GenPoly([1, 0, 1, 1])
+y   = GenPoly([0, 1, 1, 1, 0, 1, 1])
+prd = GenPoly([0, 1, 1, 0, 0, 1, 0, 1, 0, 1])
+testMult(x,y,prd) 
+print("Done.")
+
+print("Testing division...")
+
+def testDiv(x, y, expected):
+    actual = x / y
+    if not (actual.coefficients == expected.coefficients):
+        print("division failed")
+        print("x: ", x, sep='')
+        print("y: ", y, sep='')
+        print("quotient: ", actual, sep='')
+        print("expected: ", expected, sep='')
+
+x   = GenPoly([0, 1, 1, 1, 1, 1, 1])
+y   = GenPoly([1, 0, 0, 0])
+quo = GenPoly([0, 1, 1, 1, 1, 1, 1])
+testDiv(x, y, quo)
+
+x   = GenPoly([0, 1, 1, 1, 1, 1, 1])
+y   = GenPoly([1, 0, 0, 0])
+quo = GenPoly([0, 1, 1, 1, 1, 1, 1])
+testDiv(x, y, quo)
+
+x   = GenPoly([0, 1, 1, 1, 1, 1, 1])
+y   = GenPoly([0, 1, 0, 0])
+quo = GenPoly([1, 1, 1, 1, 1, 1])
+testDiv(x, y, quo)
+
+x   = GenPoly([0, 1, 1, 1, 1, 1, 1])
+y   = GenPoly([0, 0, 1, 0])
+quo = GenPoly([1, 1, 1, 1, 1])
+testDiv(x, y, quo)
+
+x   = GenPoly([1, 1, 0, 1, 0, 1, 0, 0, 1, 0])
+y   = GenPoly([1, 0, 1, 0])
+quo = GenPoly([1, 0, 1, 1, 1, 0, 1])
+testDiv(x, y, quo)
+
+print("Done.")
--- GeneralizedPoly.py
+++ GeneralizedPoly.py
@@ -1,64 +1,107 @@
+class GenPoly: # Represents a generalized polynomial with coefficients of 0 or 1
+    def __init__(self, coefficients=[0] * 8):
+        # coefficients[i] should be the coefficient of the term with degree i
+        # (EG, coefficients[0] should be the constant)
+        for i in coefficients:
+            if not i in [0,1]:
+                raise ValueError("A polynomial in GF(2^8) has coefficients of zero or one.  Actual coefficient list: " + str(coefficients))
+        self.coefficients = coefficients[:]
 
-class GenPoly:
-
-
-def m():
-    # Returns the coefficient array for the irreducible polynomial m used for multiplication
-    return [1,1,0,1,1,0,0,0,1] # 1 + x + x^3 + x^4 + x^8
+    def degree(self):
+        return lsDegree(self.coefficients)
         
-class mPoly:
-    def __init__(self):
-        self.coefficients = m()
+    def __len__(self):
+        return len(self.coefficients)
 
+    def __add__(self, other):
+        highDegree = None
+        lowDegree = None
+        if self.degree() < other.degree():
+            highDegree = other.coefficients
+            lowDegree = self.coefficients
+        else: 
+            highDegree = self.coefficients
+            lowDegree = other.coefficients
+        
+        output = highDegree[:]
+        for i in range(lsDegree(lowDegree) + 1): #We have to be careful of having a most-significant-coefficient of 0
+            output[i] ^= lowDegree[i]
+        return GenPoly(output)
+    
     def __sub__(self, other):
-        difference = mPoly()
-        for i in range(len(other.coefficients)):
-            difference.coefficients[i] ^= other.coefficients[i]
-        return Polynomial()
-        
+        # Section 4.1: "subtraction of polynomials is identical to addition of polynomials."
+        return self + other
+    
+    def __mul__(self, other):
+        # Just honking multiply the polynomials the hard way, and let EEA handle the fallout
+        productLen = len(self) + len(other)
+        product = [0] * productLen
+        for i in range(len(self.coefficients)): # Index into self.coefficients
+            for j in range(len(other.coefficients)): # Index into other.coefficients
+                term = self.coefficients[i] * other.coefficients[j]
+                index = i + j
+                product[index] ^= term # "add" (xor) terms of the same degree
+        # Problem: one of the coefficient arrays may have a most-significant-coefficient of zero
+        # Solution: trim product of trailing zeros via array slices & degree()
+        return GenPoly(product[:lsDegree(product) + 1])
+    
     def __truediv__(self, other):
         # Polynomial long division
         if not any(other.coefficients):
             raise ZeroDivisionError # No coefficient of other is nonzero means other is zero
-
-        quotient = Polynomial()
-
-        degreeDifference = degree(self.coefficients) - degree(other.coefficients)
-        quotient.coefficients[degreeDifference] = 1
-
-        # Assume `other` is a Polynomial, with degree <= 8
-        # Special handling for the first iteration, where `remainder` is m, which has too many coefficients
-
-        factorCoefficients = ([0] * degreeDifference) + other.coefficients
-        remainderCoefficients = self.coefficients[:]
-        for i in range(9):
-            remainderCoefficients[i] ^= factorCoefficients[i]
-        
-        remainder = Polynomial(remainderCoefficients[:8])
-        degreeDifference = degree(remainder.coefficients) - degree(other.coefficients)
+        quotient = GenPoly([0] * len(self.coefficients))
+        remainder = GenPoly(self.coefficients)
+        degreeDifference = remainder.degree() - other.degree()
         while degreeDifference >= 0 and any(remainder.coefficients):
+            factor = xToThe(degreeDifference)
+            remainder -= (factor * other)
             quotient.coefficients[degreeDifference] = 1
-            
-            factorCoeff = ([0] * degreeDifference) + other.coefficients
-            factor = Polynomial(factorCoeff)
-            remainder -= factor
-            degreeDifference = degree(remainder.coefficients) - degree(other.coefficients)
+            degreeDifference = remainder.degree() - other.degree()
+        quotient.trim()
         return quotient
+    
+    def inverse(self):
+        raise NotImplementedError
+        # EEA based off of wikipedia pseudocode
+        old_s = one()
+        s = zero()
+        old_t = zero()
+        t = one()
+        old_r = mPoly()
+        r = self
+        while any(r.coefficients):
+            quotient = old_r / r
+            (old_r, r) = (r, old_r - quotient * r)
+            (old_s, s) = (s, old_s - quotient * s)
+            (old_t, t) = (t, old_t - quotient * t)
+        print("Bézout coefficients:", old_s, old_t, sep='')
+        print("greatest common divisor:", old_r)
+        print("quotients by the gcd:", t, s, sep='')
 
-
+    def trim(self):
+        self.coefficients = self.coefficients[:self.degree() + 1]
+                
+            
+            
+    
     def __str__(self):
-        return "x^8 + x^4 + x^3 + x +1"
-        #
-        #remainder = Polynomial(m())
-        #degreeDifference = degree(remainder.coefficients) - degree(other.coefficients)
-        #while degreeDifference >= 0 and any(remainder.coefficients):
-        #    leftShift = ([0] * degreeDifference) + other.coefficients
-        #    factor = Polynomial(leftShift[:8])
-        #    remainder -= factor
-        #    quotient.coefficients[degreeDifference] = 1
-        #    degreeDifference = degree(remainder.coefficients) - degree(other.coefficients)
-        #return quotient
+        # Convienent string representation not described in the specification
+        output = []
+        for i in range(len(self) - 1, 1, -1): # 7, 6, 5, 4, 3, 2
+            if self.coefficients[i]:
+                output.append("x^" + str(i))
+        if self.coefficients[1]:
+            output.append("x") # Not x^1
+        if self.coefficients[0]:
+            output.append("1") # Certainly not x^0
+        if any(output):
+            return " + ".join(output)
+        else:
+            return "0"
 
+def xToThe(n):
+    leftPad = [0] * n;
+    return GenPoly(leftPad + [1])
 
 def one():
     return Polynomial([1,0,0,0,0,0,0,0])
@@ -67,7 +110,7 @@ def zero():
     return Polynomial([0,0,0,0,0,0,0,0])
 
        
-def degree(coefficients):
+def lsDegree(coefficients):
     # I can't find a simple way to find the index of last nonzero element of a list,
     # especially if the list may be all zeros
     for i in range(len(coefficients) - 1, 0, -1):
